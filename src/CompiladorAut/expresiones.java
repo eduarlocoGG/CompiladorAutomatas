7a8ed8b2954aa7673f2dd95e5b64e2480b3dff5e
Joel Rivera <rf_edd@live.com>
2020-05-07 19:01:36 -0600

    tabla de simbolos

---
+++ src/CompiladorAut/expresiones.java
@@ -0,0 +1,18 @@
+package CompiladorAut;
+
+public class expresiones {
+	String identificador;
+	String expresion;
+	public String getIdentificador() {
+		return identificador;
+	}
+	public void setIdentificador(String identificador) {
+		this.identificador = identificador;
+	}
+	public String getExpresion() {
+		return expresion;
+	}
+	public void setExpresion(String expresion) {
+		this.expresion = expresion;
+	}
+}
